4b1abb1ed8e1281d12fff395732832b4870bbd02
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2021-12-09 04:44:17 -0600

    final

---
+++ index.js
@@ -0,0 +1,17 @@
+const house = document.getElementById("house")
+const decorator = document.getElementById("decorator")
+decorator.addEventListener("click", deckTheHalls)
+
+function deckTheHalls() {
+  // Task:
+  // Write a function to add decorations (e.g. ☃️, 🎄, etc) next to the house when the switch is toggled. 
+  if (!decorator.checked) {
+    house.textContent = `🏡`;
+  } else {
+    house.textContent += `🎄` + `☃️`;
+  }
+}
+
+// Stretch goals:
+// - Make sure that the house & decorations all stay on one line, even if there are a lot of decorations.
+// - Allow the user to select different decorations.
